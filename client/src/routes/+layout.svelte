d804db587d6a0d1b6514ada44dbceacc8cabf5e3
Ngo Gia Phong <ngogiaphong123@gmail.com>
2023-07-01 11:22:59 +0700

    Project init and register, login form layout

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import 'bootstrap/dist/css/bootstrap.min.css';
+</script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="/">Home</a>
+		<div class="mr-auto d-flex">
+			<button type="button" class="login-btn btn btn-primary"
+				><a class="nav-link" href="/login">Login</a></button
+			>
+			<button type="button" class="btn btn-primary"
+				><a class="nav-link" href="/register">Register</a></button
+			>
+		</div>
+	</div>
+</nav>
+<slot />
+
+<style>
+    .login-btn {
+        margin-right: 1rem;
+    }
+</style>
---
+++ client/src/routes/register/+page.svelte
@@ -0,0 +1,27 @@
+<div class="container-fluid w-25 h-100 mt-3">
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+            />
+            <small id="emailHelp" class="form-text text-muted"
+                >We'll never share your email with anyone else.</small
+            >
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" placeholder="Password" />
+        </div>
+        <div class="form-group">
+            <label for="confirmPassword">Confirm password</label>
+            <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm password" />
+        </div>
+        <button type="submit" class="mt-2 btn btn-primary">Register</button>
+    </form>
+    
+</div>
