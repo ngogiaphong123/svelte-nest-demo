24ec97a02e37b3249c4e539d381b343503561fae
Ngo Gia Phong <ngogiaphong123@gmail.com>
2023-07-01 12:57:11 +0700

    Form actions, logic prepare for calling API

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -1,23 +1,32 @@
+<script lang="ts">
+	let email: string;
+	let password: string;
+
+	const handleLogin = async (e: Event) => {
+		e.preventDefault();
+		const formData = new FormData();
+		formData.append('email', email);
+		formData.append('password', password);
+		// Call the API
+	};
+</script>
+
 <div class="container-fluid w-25 h-100 mt-3">
-    <form>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="Enter email"
-            />
-            <small id="emailHelp" class="form-text text-muted"
-                >We'll never share your email with anyone else.</small
-            >
-        </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-        </div>
-        <button type="submit" class="mt-2 btn btn-primary">Login</button>
-    </form>
-    
+	<form>
+		<div class="form-group" on:submit|preventDefault|stopPropagation={handleLogin}>
+			<label for="email">Email address</label>
+			<input type="email" class="form-control" id="email" bind:value={email} />
+		</div>
+		<div class="form-group">
+			<label for="password">Password</label>
+			<input
+				bind:value={password}
+				type="password"
+				class="form-control"
+				id="password"
+				placeholder="Password"
+			/>
+		</div>
+		<button type="submit" class="mt-2 btn btn-primary">Login</button>
+	</form>
 </div>
